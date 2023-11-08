06fc4f7e45ca040ccd24200ac36dcef4bde492c3
obisesan olamilekan <olamilekan.obisesan1@gmail.com>
2023-11-08 05:36:20 +0100

    '''''''

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/Button";
 import React from "react";
 import { motion } from "framer-motion";
+
 function Contact() {
   return (
     <motion.div
@@ -15,10 +16,10 @@ function Contact() {
         hidden: { opacity: 0, y: 0 },
       }}
     >
-      <h2 className="contact-title">What&apos;s Next?</h2>
+      <h2 className="contact-title">What&#39;s Next?</h2>
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
-        I&apos;m always open to new opportunities, my inbox is always open. Have
+        I&#39;m always open to new opportunities, my inbox is always open. Have
         a proposal, a question or just want to say hi?, hit me up!
       </p>
       <div className="contact-cta">
